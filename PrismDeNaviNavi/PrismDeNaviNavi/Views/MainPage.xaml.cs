64dcc5271186bad3b09bdf02620f3192d071a3f2
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2016-12-27 09:07:22 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeNaviNavi/PrismDeNaviNavi/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeNaviNavi.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
